c723ecbd3cbbddb59699e48f18d5e1cf8f38080c
khalid09dev <mdkhalidhossen@hotmail.com>
2024-06-18 18:57:40 +0600

    added

--- scripts/client.js
+++ scripts/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://dummy-chat-app-server-247bp61yg-md-khalid-hossens-projects.vercel.app/');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
